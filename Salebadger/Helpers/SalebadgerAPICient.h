64cf67e5785fa10a2aeefc0f92b04aa65e15df67
Iskandar Goh <incredible2288@gmail.com>
2014-02-27 23:19:22 +0000

    added AFIncrementalStore dependencies and added initial APIClient class

---
+++ Salebadger/Helpers/SalebadgerAPICient.h
@@ -0,0 +1,13 @@
+//
+//  SalebadgerAPICient.h
+//  Salebadger
+//
+//  Created by Lunayo on 27/02/2014.
+//  Copyright (c) 2014 Codebadge. All rights reserved.
+//
+
+#import "AFRESTClient.h"
+
+@interface SalebadgerAPICient : AFRESTClient
+
+@end
---
+++ Salebadger/Helpers/SalebadgerAPICient.m
@@ -0,0 +1,13 @@
+//
+//  SalebadgerAPICient.m
+//  Salebadger
+//
+//  Created by Lunayo on 27/02/2014.
+//  Copyright (c) 2014 Codebadge. All rights reserved.
+//
+
+#import "SalebadgerAPICient.h"
+
+@implementation SalebadgerAPICient
+
+@end
